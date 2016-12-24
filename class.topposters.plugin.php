cbc5272aefc6e739e59b5a2bd0d8802a4f738205
r-j <robin.jurinka@gmx.net>
2016-12-24 09:02:58 +0100

    initial release

---
+++ class.topposters.plugin.php
@@ -0,0 +1,160 @@
+<?php
+
+$PluginInfo['topPosters'] = [
+    'Name' => 'Top Posters',
+    'Description' => 'List users with most posts in a given period. Results are cached and cache is invalidated every 2 minutes.',
+    'Version' => '0.1',
+    'HasLocale' => true,
+    'Author' => 'Robin Jurinka',
+    'AuthorUrl' => 'http://vanillaforums.org/profile/r_j',
+    'RequiredApplications' => ['Vanilla' => '>=2.3'],
+    'RegisterPermissions' => ['Plugins.TopPosters.View'],
+    'SettingsPermission' => [
+        'Garden.Settings.Manage',
+        'Garden.Community.Manage'
+    ],
+    'RequiredTheme' => false,
+    'RequiredPlugins' => false,
+    'MobileFriendly' => true,
+    'License' => 'MIT',
+];
+
+class TopPostersPlugin extends Gdn_Plugin {
+    public function base_render_before($sender) {
+        $sender->permission('Plugins.TopPosters.View');
+        if ($sender->MasterView == 'default' || $sender->MasterView == '') {
+            $sender->Menu->addLink('TopPosters', t('Top Posters'), 'vanilla/topposters');
+        }
+    }
+
+    public function vanillaController_topPosters_create($sender, $args = '') {
+        $sender->permission('Plugins.TopPosters.View');
+
+        // Find which page to show and which filter to apply.
+        $page = strtolower($args[0]);
+        $filter = 'all';
+        if (in_array($page, ['all', 'currentweek', 'previousweek'])) {
+            $filter = $page;
+            $page = val($args[1], '');
+        }
+
+        // Determine offset from $page.
+        list($offset, $limit) = offsetLimit($page, c('topPosters.usersPerPage', 30), true);
+        $page = pageNumber($offset, $limit);
+
+        // Set canonical URL.
+        $sender->canonicalUrl(url(concatSep('/', ['vanilla', 'topposters', $filter], pageNumber($offset, $limit, true, false)), true));
+
+        // Setup head.
+        $sender->setData('Title', t('Top Posters'));
+        $sender->Form = new Gdn_Form();
+        $sender->Form->setValue('Filter', $filter);
+        $sender->setData(
+            'Filter',
+            [
+                'all' => t('all time'),
+                'currentweek' => t('current week'),
+                'previousweek' => t('previous week')
+            ]
+        );
+        // Redirect based on filter
+        if ($sender->Form->isPostBack() && $sender->Form->getValue('GO!') != '') {
+            redirect(url('/vanilla/topposters/'.$sender->Form->getValue('Filter')));
+        }
+
+        // Add modules
+        $sender->addModule('DiscussionFilterModule');
+        $sender->addModule('NewDiscussionModule');
+        $sender->addModule('CategoriesModule');
+        $sender->addModule('BookmarkedModule');
+        $sender->setData('Breadcrumbs', [['Name' => t('Top Posters'), 'Url' => '/vanilla/topposters']]);
+
+        $topPosters = Gdn::cache()->get($filter.'TopPosters');
+        if ($topPosters == Gdn_Cache::CACHEOP_FAILURE) {
+            $wheres = '';
+            switch ($filter) {
+                case 'currentweek':
+                    $wheres = "WHERE DateInserted >= '".date('Y-m-d 00:00:00', strtotime('-1 week'))."'";
+                    break;
+                case 'previousweek':
+                    $wheres = 'WHERE DateInserted < ';
+                    $wheres .= "'".date('Y-m-d 00:00:00', strtotime('-1 week'))."'\r\n";
+                    $wheres .= '    AND DateInserted >= ';
+                    $wheres .= "'".date('Y-m-d 00:00:00', strtotime('-2 week'))."'\r\n";
+                    break;
+                default:
+            }
+
+            $px = Gdn::structure()->databasePrefix();
+            $sql = <<<EOS
+SELECT
+    u.Name
+    , u.UserID
+    , COALESCE(CommentCount, 0) AS `CommentCount`
+    , COALESCE(DiscussionCount, 0) AS `DiscussionCount`
+    , COALESCE(CommentCount, 0) + COALESCE(DiscussionCount, 0) AS `PostCount`
+FROM
+    {$px}User u
+LEFT JOIN (
+    SELECT InsertUserID, COUNT(CommentID) AS `CommentCount`
+    FROM GDN_Comment
+    {$wheres}
+    GROUP BY InsertUserID
+) c
+    ON u.UserID = c.InsertUserID
+LEFT JOIN (
+    SELECT InsertUserID, count(DiscussionID) `DiscussionCount`
+    FROM GDN_Discussion
+    {$wheres}
+    GROUP BY InsertUserID
+) d
+    ON u.UserID = d.InsertUserID
+WHERE
+    COALESCE(CommentCount, 0) + COALESCE(DiscussionCount, 0) > 0
+EOS;
+
+            $topPosters = Gdn::sql()->query($sql)->resultArray();
+            usort($topPosters, function ($a, $b) {
+                return $b['PostCount'] - $a['PostCount'];
+            });
+
+            Gdn::cache()->store(
+                $filter.'TopPosters',
+                $topPosters,
+                [Gdn_Cache::FEATURE_EXPIRY => 120]
+            );
+        }
+
+        $sender->setData('TopPoster', $topPosters);
+        switch ($filter) {
+            case 'currentweek':
+                $sender->setData(
+                    [
+                        'CssAllFilter' => '',
+                        'CssCurrentWeekFilter' => ' class="Active"',
+                        'CssPreviousWeekFilter' => ''
+                    ]
+                );
+                break;
+            case 'previousweek':
+                $sender->setData(
+                    [
+                        'CssAllFilter' => '',
+                        'CssCurrentWeekFilter' => '',
+                        'CssPreviousWeekFilter' => ' class="Active"'
+                    ]
+                );
+                break;
+            default:
+                $sender->setData(
+                    [
+                        'CssAllFilter' => ' class="Active"',
+                        'CssCurrentWeekFilter' => '',
+                        'CssPreviousWeekFilter' => ''
+                    ]
+                );
+        }
+
+        $sender->render($this->getView('topposters.php'));
+    }
+}
---
+++ views/topposters.php
@@ -0,0 +1,48 @@
+<?php defined('APPLICATION') or die; ?>
+<style>
+.TopPosters .MItem {display: inline-block; width: 20%;}
+.TopPosters .MItem a:first-child {margin-right: 0.5em;}
+#Form_Filter {display: inline-block; width: auto;}
+</style>
+<h1><?= t('Top Posters') ?></h1>
+
+<?= $this->Form->open(); ?>
+<p>
+<?= t('Show top posters of ') ?>
+<?= $this->Form->dropDown('Filter', $this->data('Filter')) ?>
+<?= $this->Form->button('GO!') ?>
+</p>
+<?= $this->Form->close() ?>
+<?= hoverHelp('string', 'help') ?>
+
+<ul class="DataList TopPosters">
+    <li class="Item Title">
+        <span class="MItem"><?= t('User') ?></span>
+        <span class="MItem"><?= t('Comments') ?></span>
+        <span class="MItem"><?= t('Discussions') ?></span>
+        <span class="MItem"><?= t('Sum') ?></span>
+    </li>
+    <?php
+    foreach ($this->data('TopPoster') as $poster) {
+        $user = Gdn::userModel()->getID($poster['UserID']);
+    ?>
+    <li class="Item">
+        <span class="MItem">
+            <?php
+            if (c('Vanilla.Comment.UserPhotoFirst', true)) {
+                echo userPhoto($user, ['Size' => 'Small']).userAnchor($user, 'Username');
+            } else {
+                echo userAnchor($user, 'Username').userPhoto($user, ['Size' => 'Small']);
+            }
+            ?>
+        </span>
+        <span class="MItem">
+            <?= anchor($poster['CommentCount'], userUrl($user, '', 'comments')) ?>
+        </span>
+        <span class="MItem">
+            <?= anchor($poster['DiscussionCount'], userUrl($user, '', 'discussions')) ?>
+        </span>
+        <span class="MItem"><?= $poster['PostCount'] ?></span>
+    </li>
+    <?php } ?>
+</ul>
